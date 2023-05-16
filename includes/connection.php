25aa9dfeef3716699be3514662c43c3f95778b81
Jeyym Santos <santosjl@students.nu-baliwag.edu.ph>
2023-05-16 23:06:25 +0800

    pc changes

--- includes/connection.php
+++ includes/connection.php
@@ -6,11 +6,11 @@ $server_password = "";
 $server_database = "carpool_app";
 $home = "http://localhost/2ND%20YEAR/3RD%20TERM/carpool-app/";
 
-$server = "localhost";
-$server_username = "u235219407_jeyymsantos";
-$server_password = "Jeyym@15";
-$server_database = "u235219407_carpool_app";
-$home = "https://carpool.jeyymsantos.com";
+// $server = "localhost";
+// $server_username = "u235219407_jeyymsantos";
+// $server_password = "Jeyym@15";
+// $server_database = "u235219407_carpool_app";
+// $home = "https://carpool.jeyymsantos.com";
 
 $connection = mysqli_connect($server, $server_username, $server_password, $server_database);
 session_start();
