0b033d64341de63b0a6f7fc5f91609a6f24cc04c
Jeyym Santos <santosjl@students.nu-baliwag.edu.ph>
2023-06-12 02:12:55 +0800

    Establishing Admin Layout

---
+++ admin/components/foot.php
@@ -0,0 +1,41 @@
+            <!-- footer -->
+            <!-- ============================================================== -->
+            <footer class="footer text-center text-muted">
+                Making your daily Filipino trips convenient. Sabay App.
+            </footer>
+            <!-- ============================================================== -->
+            <!-- End footer -->
+            <!-- ============================================================== -->
+        </div>
+        <!-- ============================================================== -->
+        <!-- End Page wrapper  -->
+        <!-- ============================================================== -->
+    </div>
+    <!-- ============================================================== -->
+    <!-- End Wrapper -->
+    <!-- ============================================================== -->
+    <!-- End Wrapper -->
+    <!-- ============================================================== -->
+    <!-- All Jquery -->
+    <!-- ============================================================== -->
+    <script src="../assets/backend/assets/libs/jquery/dist/jquery.min.js"></script>
+    <script src="../assets/backend/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- apps -->
+    <!-- apps -->
+    <script src="../assets/backend/dist/js/app-style-switcher.js"></script>
+    <script src="../assets/backend/dist/js/feather.min.js"></script>
+    <script src="../assets/backend/assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js"></script>
+    <script src="../assets/backend/dist/js/sidebarmenu.js"></script>
+    <!--Custom JavaScript -->
+    <script src="../assets/backend/dist/js/custom.min.js"></script>
+    <!--This page JavaScript -->
+    <script src="../assets/backend/assets/extra-libs/c3/d3.min.js"></script>
+    <script src="../assets/backend/assets/extra-libs/c3/c3.min.js"></script>
+    <script src="../assets/backend/assets/libs/chartist/dist/chartist.min.js"></script>
+    <script src="../assets/backend/assets/libs/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js"></script>
+    <script src="../assets/backend/assets/extra-libs/jvector/jquery-jvectormap-2.0.2.min.js"></script>
+    <script src="../assets/backend/assets/extra-libs/jvector/jquery-jvectormap-world-mill-en.js"></script>
+    <script src="../assets/backend/dist/js/pages/dashboards/dashboard1.min.js"></script>
+</body>
+
+</html>
--- includes/connection.php
+++ includes/connection.php
@@ -5,13 +5,13 @@ $server_username = "root";
 $server_password = "";
 $server_database = "carpool_app";
 $home = "http://localhost/2ND YEAR/3RD TERM/carpool-app";
-$home = "http://localhost/carpool-app";
+// $home = "http://localhost/carpool-app";
 
-$server = "localhost";
-$server_username = "u235219407_jeyymsantos";
-$server_password = "Jeyym@15";
-$server_database = "u235219407_carpool_app";
-$home = "https://carpool.jeyymsantos.com";
+// $server = "localhost";
+// $server_username = "u235219407_jeyymsantos";
+// $server_password = "Jeyym@15";
+// $server_database = "u235219407_carpool_app";
+// $home = "https://carpool.jeyymsantos.com";
 
 $connection = mysqli_connect($server, $server_username, $server_password, $server_database);
 session_start();
